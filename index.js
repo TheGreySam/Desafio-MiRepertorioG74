b7bb32507f48beee54070e00d910002d084e97ed
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2025-01-25 18:47:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+
+const { home, leerCanciones, insertarCancion, BorrarCancion, editarCancion } = require('./controller/component');
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+app.get('/', home);
+app.get('/canciones', leerCanciones);
+app.post('/canciones', insertarCancion);
+app.delete('/canciones/:id', BorrarCancion);
+app.put('/canciones/:id', editarCancion);
+
+app.listen(port, console.log(`Servidor corriendo en ${port}`));
